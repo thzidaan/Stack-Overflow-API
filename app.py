247a0b0874ff08c97a854d9122592b920538a3b3
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-03-06 21:06:10 -0600

    Collapsible Fixed

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import time
 
 app = Flask(__name__)
 
-# (1615523017 - 1614918217) (March 11 - March 4 ==> 1 week)
+# (1615523017 - 1614918217) (i.e March 11 - March 4 ==> 1 week)
 seven_days_time = 604800
 current_Time = int(time.time())  # Gets Current Time
 # Sets from date for the API request
 week_earlier_time = str(current_Time - seven_days_time)
 
 # post_id == question_id --> Remember
+# Use Search API
 
 post_endpoint = 'https://api.stackexchange.com/2.2/posts'  # Will be used for comments
 question_endpoint = 'https://api.stackexchange.com/2.2/questions'
